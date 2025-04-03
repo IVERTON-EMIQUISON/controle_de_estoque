a1214aebd22ee3d49b89cce53bdd6ada35444f37
IVERTON <100052186+IVERTON-EMIQUISON@users.noreply.github.com>
2025-04-02 21:18:52 -0300

    atualização do banco

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for, f
 from flask_sqlalchemy import SQLAlchemy # type: ignore
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user, login_manager # type: ignore
 from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
+from flask_cors import CORS # type: ignore
 
 app = Flask(__name__)
+CORS(app) # Isso libera o CORS para todas as rotas
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estoque.db'  # Armazena no /tmp/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secreta_chave_aqui'  # Defina uma chave secreta
